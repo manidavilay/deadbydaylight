ba71c40287934d9f86b8428b22888ea53859b7d0
Manida Vilay <vilaymanida@gmail.com>
2022-08-16 15:12:19 +0200

    add scroll up btn

--- components/UI/BaseList.vue
+++ components/UI/BaseList.vue
@@ -4,8 +4,28 @@
             <slot name="title"></slot>
         </h1>
         <slot name="content"></slot>
+        <v-btn fixed icon class="list__scroll-up" @click="scrollToTop()">
+          <v-icon size="30px" class="pa-2">
+            {{ scrollUp }}
+          </v-icon>
+        </v-btn>
     </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrollUp: "mdi-arrow-up",
+    }
+  },
+  methods: {
+    scrollToTop() {
+      window.scrollTo(0,0)
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 .list {
@@ -20,6 +40,20 @@
 
     @media all and (max-width: 600px) {
       font-size: 30px;
+    }
+  }
+
+  &__scroll-up {
+    bottom: 50px;
+    right: 130px;
+    border: 1px solid #FFFFFF;
+
+    @media all and (max-width: 960px) {
+      right: 70px;
+    }
+
+    @media all and (max-width: 600px) {
+      right: 15px;
     }
   }
 }
